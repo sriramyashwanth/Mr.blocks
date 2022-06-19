daaa0b48d8c8e4c424ea597cb966affc086ab848
sriramyashwanth <38062182+sriramyashwanth@users.noreply.github.com>
2022-06-19 16:01:03 +0530

    Added script to the player and sprites
    Added script to the player to give controls to the Player and 2 sprites 1st is Finish door and the another one is obstacle walls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D Controls;
+
+    public float speed;
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            Controls.velocity = new Vector2(speed, 0f);
+        }
+
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            Controls.velocity = new Vector2(-speed, 0f);
+        }
+
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            Controls.velocity = new Vector2(0f, speed);
+        }
+
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            Controls.velocity = new Vector2(0f, -speed);
+        }
+
+        else if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            Controls.velocity = new Vector2(0f, 0f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Finish")
+        {
+            Debug.Log("level complaeted");
+        }
+        
+    }
+}
